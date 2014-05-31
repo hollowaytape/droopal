9669e8ec29e02dbd5feb53f5f2d5868f91d1e4d0
Max Silbiger <tsmaxweliot@gmail.com>
2014-05-30 21:38:10 -0400

    Adds sqlite db.

--- droop/models.py
+++ droop/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 
 class Tree(models.Model):
-    id = IntegerField()
-    threshold = IntegerField()
-    ripeness = BooleanField()
+    id = models.IntegerField(primary_key=True)
+    threshold = models.IntegerField()
+    ripeness = models.BooleanField()
     
-class Reading(models.Model):=
-    id = models.ForeignKey(Tree)
-    value = models.IntegerField()
+    def __unicode__(self):
+        return self.id
+    
+class Reading(models.Model):
+    tree = models.ForeignKey(Tree)
     hour = models.DateTimeField()
+    value = models.IntegerField()
+    
+    def __unicode__(self):
+        return "%s-%s-%s" % (self.id, self.hour, self.value)
--- droopal/settings.py
+++ droopal/settings.py
@@ -11,8 +11,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'C:/Users/Antpile/Code/droopal/droop.db',
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
